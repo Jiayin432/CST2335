1aefe981482203b2985af218c5744266a11ba7ce
Jiayin <liu00366@algonquinlive.com>
2019-02-05 16:17:48 -0500

    Lab03-4

--- app/src/main/java/com/jiayin/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/jiayin/androidlabs/ProfileActivity.java
@@ -1,15 +1,11 @@
 package com.jiayin.androidlabs;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.graphics.Bitmap;
-import android.widget.EditText;
 import android.provider.MediaStore;
 
 public class ProfileActivity extends AppCompatActivity {
